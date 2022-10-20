7906d10037fb4908fe2599f8463e2ae8ea62248d
ekaterina-chern <113055918+ekaterina-chernykh@users.noreply.github.com>
2022-10-20 04:05:07 +0000

    done

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Created by: Ekaterina
+// Created on: Oct 2022
+// This file contains the JS function for index.html
+
+"use strict"
+/**
+ * This function calculates XXX.
+ */
+function calculate() {
+  // input
+  const worked = parseFloat(document.getElementById("number-of-hours").value)
+  const rate = parseFloat(document.getElementById("hourly-rate").value)
+
+  // process
+  const pay = worked * rate
+  const tax = pay * .13
+
+  // output
+  document.getElementById("pay").innerHTML = "Your pay will be $" + pay.toFixed(2)
+  document.getElementById("tax").innerHTML = "The govenment will take $" + tax.toFixed(2)
+}
